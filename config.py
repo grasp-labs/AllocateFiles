8cdc2ad2340cfa093ed142ecd1603bc3539bbb21
skerve <arveskjervheim@gmail.com>
2022-06-23 14:47:41 +0200

    url fix

--- config.py
+++ config.py
@@ -15,7 +15,7 @@ class Config:
     client_secret: AnyStr = os.environ.get("GRASP_DEMO_CLIENT_SECRET")
     grant_type: AnyStr = "client_credentials"
     token_url: AnyStr = "https://auth-dev.grasp-daas.com/oauth/token/"
-    service_url: AnyStr = "http://localhost:8000/api/subscription/v1/allocate/upload-file/"# "https://grasp-daas.com/api/subscription/v1/allocate/upload-file/"
+    service_url: AnyStr = "https://grasp-daas.com/api/subscription-dev/v1/allocate/upload-file/"
 
 
 configuration = Config()
